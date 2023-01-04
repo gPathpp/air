dc050885feae8cf2eef2ccb0aca4e1114ede3b71
Gaisberger Patrick <patrick.gaisberger@student.tugraz.at>
2023-01-04 21:33:39 +0100

    predict artist v1

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
+from typing import Dict, Tuple
+
 import numpy as np
+import pandas as pd
 import torch
+from pandas import DataFrame
+from torch.nn.functional import one_hot
+from torch.utils.data import DataLoader
 
 from model import DenseNet
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def replace_artist_with_id(data: DataFrame) -> Dict[int, str]:
+    artist_dict = {artist_id: artist for artist_id, artist in enumerate(set(data.artist))}
+    id_dict = {artist_dict[artist_id]: artist_id for artist_id in artist_dict.keys()}
+    data.artist = data.artist.apply(lambda artist: id_dict[artist])
+    return artist_dict
+
+def train(dataloader, model, loss_fn, optimizer):
+    model.train()
+    epoch_loss = 0
+    for current_token, target in dataloader:
+        optimizer.zero_grad()
+        out = torch.softmax(model.forward(current_token), dim=1)
+        batch_loss = loss_fn(out, one_hot(target, model.outputs).float())
+        batch_loss.backward()
+        epoch_loss += batch_loss.item()
+        optimizer.step()
+        accuracy = torch.sum(torch.argmax(out, dim=1) == target) / dataloader.batch_size
+        print(f"Batch-Loss: {batch_loss.item()}")
+        print(f"Batch-Accuracy: {accuracy}")
+    print(f"Train epoch loss: {epoch_loss / len(dataloader)}")
+
 
 if __name__ == "__main__":
-    inputs = 100
-    model = DenseNet(inputs, outputs=100)
-    optimizer = torch.optim.Adagrad(model.parameters(), lr=0.01, weight_decay=0.02)
+    data = pd.read_csv("spotify_millsongdata.csv")
+    artist_dict = replace_artist_with_id(data)
+    model = DenseNet(outputs=len(artist_dict))
+    optimizer = torch.optim.Adagrad(model.parameters(), lr=0.1, weight_decay=0.02)
     loss_fn = torch.nn.CrossEntropyLoss()
-    model.eval()  # set into eval mode TODO remove
-    print(model.forward(torch.from_numpy(np.random.random(inputs))))
+    targets = data.artist.to_numpy()
+    texts = data.text.to_numpy()
+    train_dataloader = DataLoader(list(zip(texts, targets)), shuffle=True, sampler=None, batch_size=3000)
+    train(train_dataloader, model, loss_fn, optimizer)
--- model.py
+++ model.py
@@ -1,23 +1,29 @@
 import torch
+from sentence_transformers import SentenceTransformer
 from torch import nn
 
 
 class DenseNet(nn.Module):
-    def __init__(self, inputs: int, outputs: int):
+    def __init__(self, outputs: int):
         """
         :param inputs: length of input vector
         :param outputs: number of classes
         """
         super(DenseNet, self).__init__()
-
+        self.sentence_transformer = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+        self.outputs = outputs
+        hidden_dim = 1000
         self.net = nn.Sequential(
-            nn.Linear(inputs, 125),
+            nn.Linear(self.sentence_transformer.get_sentence_embedding_dimension(), hidden_dim),
             nn.LeakyReLU(),
-            nn.Linear(125, 125),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.LeakyReLU(),
-            nn.Linear(125, outputs),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.LeakyReLU(),
+            nn.Linear(hidden_dim, outputs),
             nn.Softmax(dim=0)
         )
 
     def forward(self, inputs_):
-        return self.net(inputs_.float())
+        in_ = self.sentence_transformer.encode(inputs_)
+        return self.net(torch.from_numpy(in_))
